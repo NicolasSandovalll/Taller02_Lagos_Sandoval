c390dda8eee54ed5e49caaab8b4ea25f55e19caf
nicolas <n.sandoval13@ufromail.cl>
2024-10-11 09:50:33 -0300

    Se reinician las habitaciones al estado disponible con el metodo reiniciarHotel

--- src/main/java/Hotel.java
+++ src/main/java/Hotel.java
@@ -1,3 +1,43 @@
+import java.util.Scanner;
+
 public class Hotel {
-    
+
+    private static Scanner scanner = new Scanner(System.in);
+    private static String[] habitaciones = new String[10];
+    private static int[] nochesReservadas = new int[10];
+    private static boolean[] conAlimentacion = new boolean[10];
+    private static final String clave = "resetearTodo";
+    private static boolean[] pagada = new boolean[10]; // Indica si la habitación fue pagada o no
+
+    public static void main(String[] args) {
+        inicializarHotel();
+    }
+
+    public static void inicializarHotel() {
+        for (int i = 0; i < 10; i++) {
+            habitaciones[i] = "D"; // La D quiere decir que la habitacion esta disponible
+            nochesReservadas[i] = 0;
+            conAlimentacion[i] = false;
+        }
+        System.out.println("El hotel ha sido inicializado.");
+    }
+
+
+    // Se reinician las habitaciones a estado de disponible
+    public static void reiniciarHotel(String claveIngresada) {
+        if (claveIngresada.equals(clave)) {
+            for (int i = 0; i < 10; i++) {
+                habitaciones[i] = "D";
+                nochesReservadas[i] = 0;
+                conAlimentacion[i] = false;
+                pagada[i] = false;
+            }
+            System.out.println("El hotel ha sido reiniciado.");
+        } else {
+            System.out.println("Clave incorrecta.");
+        }
+    }
+
+
+
 }
